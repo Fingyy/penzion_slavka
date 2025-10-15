42f050ed6b264b2460b1eee9957370648d1f832a
Michael Vojtech <michael.vojtech@gmail.com>
2025-10-15 22:04:35 +0200

    feat: implementation  ligtbox for Fotogalerie page

--- penzion/static/js/main.js
+++ penzion/static/js/main.js
@@ -43,3 +43,26 @@ function showLightbox(index) {
   currentIndex = index;
   counter.textContent = `${index + 1} / ${images.length}`;
 }
+
+// i zde odpovidí index co je zmíněný výše...
+images.forEach((img, i) => {
+  img.addEventListener('click', () => showLightbox(i));
+});
+
+closeBtn.addEventListener('click', () => lightbox.style.display = 'none');
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  showLightbox(currentIndex);
+});
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showLightbox(currentIndex);
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {lightbox.style.display = 'none';}
+  if (e.key === 'ArrowRight') {nextBtn.click()}
+  if (e.key === 'ArrowLeft') {prevBtn.click()}
+})
