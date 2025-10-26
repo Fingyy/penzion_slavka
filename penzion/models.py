28e97e3858f85df702331b46540d8762436478e0
Michael Vojtech <michael.vojtech@gmail.com>
2025-10-26 20:55:31 +0100

    fix: reservation page + starting with order backend

--- penzion/models.py
+++ penzion/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class RoomType(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=50)
+    arrive_date = models.DateField()
+    departure_date = models.DateField()
+    no_of_nights = models.IntegerField()
+    no_of_adults = models.IntegerField()
+    no_of_children = models.IntegerField()
+    no_of_rooms = models.IntegerField()
+    room_type = models.ManyToManyField(RoomType)
+    description = models.TextField()
